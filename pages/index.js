da3c35232dc54b3e7066316bead519bdf8827db1
Aylin Morales <aylinmorales@Aylins-MacBook-Pro.local>
2023-09-07 18:45:09 -0700

    head title changed

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="p-2">
       <Head>
-        <title>John Doe - Software Developer</title>
+        <title>Aylin Morales</title>
         <meta 
           name="description" 
           content="Software developer portfolio using Next.js & tailwind CSS." />
